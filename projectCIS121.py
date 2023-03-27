fbe2492c264a040b14acd45422542e5d8de54ce3
Tanimoune <125086391+Tanimoune@users.noreply.github.com>
2023-03-27 17:56:18 -0500

    Add files via upload

---
+++ projectCIS121.py
@@ -0,0 +1,24 @@
+# Dictionnaries for Items, Username + Passwords
+
+items = {
+    1: {"name": "T-Shirt", "price": 20.99, "size": ["S", "M", "L"], "color": ["red", "blue", "green"], "quantity": 100},
+    2: {"name": "Hoodie", "price": 39.99, "size": ["S", "M", "L", "XL"], "color": ["black", "gray", "navy"], "quantity": 50},
+    3: {"name": "Jeans", "price": 49.99, "size": ["28", "30", "32", "34"], "color": ["blue", "black"], "quantity": 75},
+    4: {"name": "Shoes", "price": 89.99, "size": ["8", "9", "9.5", "10", "11", "12"], "color": ["blue", "black"], "quantity": 49},
+    5: {"name": "Short", "price": 19.99, "size": ["S", "M", "L", "XL", "2XL"], "color": ["blue", "black", "red", "yellow"], "quantity": 120},
+}
+
+usernames = {
+    "Miguel222": {"first_name": "Miguel", "last_name": "Sumo", "email": "Miguel@gmail.com"},
+    "Aboubacar345": {"first_name": "Aboubacar", "last_name": "Tanimoune", "email": "Aboubacar@gmail.com"},
+    "Lado111": {"first_name": "Lado", "last_name": "Lado", "email": "Lado@gmail.com"},
+    "Arbab222": {"first_name": "Arbab", "last_name": "Arbab", "email": "Arbab@gmail.com"},
+    }
+    
+passwords = {
+    "Miguel222": "password1",
+    "Aboubacar345": "password2",
+    "Lado111": "password3",
+    "Arbab222": "password4",
+}
+
